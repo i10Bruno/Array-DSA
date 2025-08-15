b1896e58ac3042d2572025100dd2e1fe4407aede
Bruno alexandre <brunosantosalexandre@hotmail.com>
2025-08-15 20:37:27 -0300

    FUNÇÃO insertAtEnd IMPLANTADA

--- src/Main.java
+++ src/Main.java
@@ -1,10 +1,34 @@
+import java.util.Arrays;
+
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
+
+    public static int[] insertAtEnd(int[] A, int currentLength,int x) {
+        A[currentLength] = x;
+        currentLength++;
+        return A;
+    }
+
+
+
     public static void main(String[] args) {
         //array
+        int[]Numbers = new  int [5];
+
+        int currentLenght=0;
 
 
+        for (int i=0;i< 2; i++){
+
+            Numbers[i]=i+100;
+            currentLenght++;
+
+        }
+        System.out.println(Arrays.toString(Numbers));
+        insertAtEnd(Numbers, currentLenght, 100);
+        System.out.println(Arrays.toString(Numbers));
+        System.out.println(currentLenght);
 
 
     }
